594f3956727761156a364b5c69898b956cd97e5a
Aleksandr Madzharov <madzharov-aleks@mail.ru>
2022-02-10 18:30:19 +0500

    add Post

---
+++ src/main/java/ry/netology/domain/Post.java
@@ -0,0 +1,17 @@
+package ry.netology.domain;
+
+public class Post {
+
+    private String name;
+    private String id;
+    private String viewsInfo;
+    private String likesInfo;
+    private String repostInfo;
+    private String commentsInfo;
+    private String header;
+    private String releaseDate;
+    private String shortDescription;
+    private String numberOfSubscribers;
+    private String postPhoto;
+    private String geoData;
+}
