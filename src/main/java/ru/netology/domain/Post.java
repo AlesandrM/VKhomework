129c0cd896f26e32d7b480e5d8c2c4153aceb171
Aleksandr Madzharov <madzharov-aleks@mail.ru>
2022-02-25 14:28:02 +0500

    add Class

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,25 @@
+package ru.netology.domain;
+
+public class Post {
+
+    private int id;
+    private int ownerId;
+    private int fromId;
+    private int createdBy;
+    private int date;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;
+    private int signerId;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private int markedAsAds;
+    private boolean isFavorite;
+    private int postponedId;
+
+
+}
+
+
